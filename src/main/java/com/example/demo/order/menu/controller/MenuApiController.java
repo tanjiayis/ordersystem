c661232e728a09f0854432f8e5e65cfd300b2bbc
tanjys <tanjys@qq.com>
2019-03-26 23:10:04 +0800

    修改文件

--- src/main/java/com/example/demo/order/menu/controller/MenuApiController.java
+++ src/main/java/com/example/demo/order/menu/controller/MenuApiController.java
@@ -2,11 +2,9 @@ package com.example.demo.order.menu.controller;
 
 import com.example.demo.controller.BaseApiController;
 import com.example.demo.data.CommonResult;
-import com.example.demo.exception.BusinessException;
 import com.example.demo.order.menu.dto.AddMenuDto;
 import com.example.demo.order.menu.dto.AddTypeDto;
 import com.example.demo.order.menu.dto.DetailDto;
-import com.example.demo.order.menu.dto.UploadDto;
 import com.example.demo.order.menu.pojo.Menu;
 import com.example.demo.order.menu.pojo.MenuType;
 import com.example.demo.exception.IllegalArgumentException;
@@ -20,15 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
 
 /**
  * Created by JIAYI_TAN on 2019/3/5.
@@ -87,54 +78,6 @@ public class MenuApiController extends BaseApiController {
         menuService.deleteType(dto.getId());
         return new CommonResult();
     }
-//    @RequestMapping("/uploader")
-//    public CommonResult uploader(HttpServletResponse response, HttpServletRequest request, UploadDto dto,
-//                                 @RequestParam(value = "name", required = false) String name,
-//                                 @RequestParam(value = "uuid", required = false) String uuid,
-//                                 @RequestParam(value = "chunk", defaultValue = "0") int chunk,
-//                                 @RequestParam(value = "chunks", defaultValue = "1") int chunks,
-//                                 @RequestHeader(value = "adddate", required = false) Integer addDate,
-//                                 @RequestParam(value = "file", required = false) MultipartFile file){
-//
-//        File targetFile=null;
-//        String url = "";
-//        int code=1;
-//        String fileName = file.getOriginalFilename();
-//        if(fileName!=null&&fileName!=""){
-////            String returnUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() +"/upload/imgs/";//存储路径
-////            System.out.println(returnUrl);
-////            String path = request.getSession().getServletContext().getRealPath("upload/imgs"); //文件存储位置
-////            String fileF = fileName.substring(fileName.lastIndexOf("."), fileName.length());//文件后缀
-//            fileName=new Date().getTime()+"_"+new Random().nextInt(1000)+fileF;//新的文件名
-//
-//            //先判断文件是否存在
-////            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-////            String fileAdd = sdf.format(new Date());
-//            //获取文件夹路径
-////            File file1 =new File(path+"/"+fileAdd);
-//            //如果文件夹不存在则创建
-////            if(!file1 .exists()  && !file1 .isDirectory()){
-////                file1 .mkdir();
-//            }
-//            //将图片存入文件夹
-//            targetFile = new File("D:\\My_work\\ordersystem\\src\\main\\resources\\static\\images\\menus", fileName);
-//            try {
-//                //将上传的文件写到服务器上指定的文件。
-//                file.transferTo(targetFile);
-////                url=returnUrl+fileAdd+"/"+fileName;
-//                code=0;
-////                result.setCode(code);
-////                result.setMessage("图片上传成功");
-////                map.put("url", url);
-////                result.setResult(map);
-//                return new CommonResult(url);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//               throw new BusinessException("系统异常！");
-//            }
-//        }
-//        return new CommonResult();
-//    }
     @RequestMapping("/uploader")
     public CommonResult uploader2(@RequestParam(value = "name", required = false) String name,
                                  @RequestParam(value = "uuid", required = false) String uuid,
@@ -143,9 +86,6 @@ public class MenuApiController extends BaseApiController {
                                  @RequestHeader(value = "adddate", required = false) Integer addDate,
                                  @RequestParam(value = "file", required = false) MultipartFile file){
         String fileName = file.getOriginalFilename();
-//        System.out.println(fileName);
-//        String fileF = fileName.substring(fileName.lastIndexOf("."), fileName.length());//文件后缀
-//        fileName=new Date().getTime()+"_"+new Random().nextInt(1000)+fileF;//新的文件名
         String serverpath= "";
         String path = "";
         try {
@@ -153,10 +93,8 @@ public class MenuApiController extends BaseApiController {
             path=serverpath.substring(1);
             File targetFile = new File(path, fileName);
             file.transferTo(targetFile);
-//            menuService.addMenuImg(fileName);
         } catch (Exception e) {
             e.printStackTrace();
-//            throw new BusinessException("图片上传失败！");
         }
         return new CommonResult();
     }
